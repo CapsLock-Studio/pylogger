5e70c6a01e0aab595a044da897ec44e2285c9371
michael34435 <michael34435@gmail.com>
2015-10-20 10:24:54 +0800

    fix

--- echo.py
+++ echo.py
@@ -1,6 +1,9 @@
 import bcolors
 
-def text(text=None, bold=False, underline=False, level='DEFAULT'):
-    if text is None: raise AttributeError('text attribute cannot be empty, it should be \'str\'')
+
+def text(text=None, bold=False, underline=False, level=bcolors.DEFAULT):
+    if text is None:
+        raise AttributeError('text attribute cant be empty, it should be "str"')
+
     level = getattr(bcolors, level) if hasattr(bcolors, level) else bcolors.DEFAULT
-    print level + (bcolors.BOLD if bold else '') +  (bcolors.underline if underline else '') + text + bcolors.ENDC
+    print level + (bcolors.BOLD if bold else '') + (bcolors.underline if underline else '') + text + bcolors.ENDC
--- logger.py
+++ logger.py
@@ -1,22 +1,29 @@
-import echo, bcolors
+import echo
+
 
 def info(text, bold=False, underline=False):
     echo.text(text=text, level='INFO', bold=bold, underline=underline)
 
+
 def notice(text, bold=False, underline=False):
     echo.text(text=text, level='NOTICE', bold=bold, underline=underline)
+
 
 def warning(text, bold=False, underline=False):
     echo.text(text=text, level='WARNING', bold=bold, underline=underline)
 
+
 def fail(text, bold=False, underline=False):
     echo.text(text=text, level='FAIL', bold=bold, underline=underline)
+
 
 def success(text, bold=False, underline=False):
     echo.text(text=text, level='SUCCESS', bold=bold, underline=underline)
 
+
 def default(text=None, bold=False, underline=False):
     echo.text(text=text, level='DEFAULT', bold=bold, underline=underline)
+
 
 def log(text=None, level=None, bold=False, underline=False):
     echo.text(text=text, level=level, bold=bold, underline=underline)
